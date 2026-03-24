a2ef4ede5706837856f2dac01db5798e0313f914
Lokesh-5618 <lokeshsaran69420@gmail.com>
2026-03-24 14:40:20 +0530

    fix case sensitivity

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router'
 function Navbar() {
     return (
         <div className="nav">
-            <Link to="/shop-page">Home</Link>
+            <Link to="/Shop-page">Home</Link>
             <div className="navright">
                 <Link to="/shop">Shop</Link>
                 <Link to="/cart">Cart</Link>
